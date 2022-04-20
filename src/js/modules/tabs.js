667c82d8c76caace200d936f59aea4a909129743
Tolstov Dmitrij <tolstov.dmitrij42@jmail.com>
2022-04-20 12:22:52 +0400

    refactoring

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,43 +1,30 @@
-export const showTabs = (
+export const showTabs = ({
    headerSelector,
    tabSelector,
    contentSelector,
    activeClass
+}
 ) => {
    const header = document.querySelector(headerSelector),
       tabs = document.querySelectorAll(tabSelector),
       contents = document.querySelectorAll(contentSelector);
-
    //   функция скрытия контентв
    function hideTabContent() {
       //    перебор контента и скытие его
-      contents.forEach((content) => {
-         content.style.display = 'none';
-      });
+      contents.forEach((content) => (content.style.display = 'none'));
       //   скрытие актиивной подложки
-      tabs.forEach((tab) => {
-         tab.classList.remove(activeClass);
-      });
+      tabs.forEach((tab) => tab.classList.remove(activeClass));
    }
-
-
-//   ! ошибка
-
    //   перебор псевдомассива с контентом
-   function showTabContent(i) {
+   function showTabContent(i = 0) {
       //    показ контента активного таба
       contents[i].style.display = 'block';
       //   contents[i].style.display = 'block';
       //   показ активной подложки
       tabs[i].classList.add(activeClass);
    }
-
    hideTabContent();
    showTabContent();
-
-
-   //   ! ошибка
-
    //    делегирование событий
    //    прослушка на общего родителя всех табов
    header.addEventListener('click', (e) => {
@@ -60,4 +47,7 @@ export const showTabs = (
          });
       }
    });
+
+   
+
 };
