aab36598296521b950fbd83c29a0b09de5fe54a2
Tolstov Dmitrij <tolstov.dmitrij42@jmail.com>
2022-04-23 14:46:39 +0400

    function to check the click inside the window

---
+++ src/js/modules/changeModalState.js
@@ -0,0 +1,78 @@
+import { checkNumInputs } from './checkNumInputs';
+
+export const changeModalState = (state) => {
+   // форма окна калькулятора
+   const windowForms = document.querySelectorAll('.balcon_icons_img'),
+      windowWidths = document.querySelectorAll('#width'),
+      windowHeights = document.querySelectorAll('#height'),
+      windowTypes = document.querySelectorAll('#view_type'),
+      windowProfiles = document.querySelectorAll('.checkbox');
+
+   checkNumInputs('#width');
+   checkNumInputs('#height');
+
+   function bindActionToElems({ event, elems, prop }) {
+      elems.forEach((elem, i) => {
+         elem.addEventListener(event, () => {
+            //  switch - для проверки,куда кликнул пользователь
+            switch (elem.nodeName) {
+               case 'SPAN':
+                  state[prop] = i;
+                  break;
+               case 'INPUT':
+                  if (elem.getAttribute('type') === 'checkbox') {
+                     i === 0
+                        ? (state[prop] = 'Холодное')
+                        : (state[prop] = 'Теплое');
+                     elems.forEach((box, j) => {
+                        box.checked = false;
+                        if (i == j) {
+                           box.checked = true;
+                        }
+                     });
+                  } else {
+                     state[prop] = elem.value;
+                  }
+                  break;
+               case 'SELECT':
+                  state[prop] = elem.value;
+                  break;
+            }
+            console.log(state);
+         });
+      });
+   }
+
+   const windowForm = {
+      event: 'click',
+      elems: windowForms,
+      prop: 'form',
+   };
+   const windowWidth = {
+      event: 'input',
+      elems: windowWidths,
+      prop: 'width',
+   };
+   const windowHeight = {
+      event: 'input',
+      elems: windowHeights,
+      prop: 'height',
+   };
+
+   const windowType = {
+      event: 'change',
+      elems: windowTypes,
+      prop: 'type',
+   };
+   const windowProfile = {
+      event: 'change',
+      elems: windowProfiles,
+      prop: 'profile',
+   };
+
+   bindActionToElems(windowForm);
+   bindActionToElems(windowWidth);
+   bindActionToElems(windowHeight);
+   bindActionToElems(windowType);
+   bindActionToElems(windowProfile);
+};
