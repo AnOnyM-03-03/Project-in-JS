c389ed72c738bb0daaf31cbb23688b9d14c9caa0
Tolstov Dmitrij <tolstov.dmitrij42@jmail.com>
2022-04-24 16:55:17 +0400

    added display of images on the screen

---
+++ src/js/modules/images.js
@@ -0,0 +1,36 @@
+export const images = () => {
+   const imgPopup = document.createElement('div'),
+      workSection = document.querySelector('.works'),
+      bigImage = document.createElement('img');
+
+   imgPopup.classList.add('popup');
+   workSection.appendChild(imgPopup);
+   imgPopup.appendChild(bigImage);
+
+   imgPopup.style.justifyContent = 'center';
+   imgPopup.style.alignItems = 'center';
+   imgPopup.style.display = 'none';
+
+   function popupDisplayFlex() {
+      imgPopup.style.display = 'flex';
+      document.body.style.overflow = 'hidden';
+   }
+   function popupDisplayNone() {
+      imgPopup.style.display = 'none';
+      document.body.style.overflow = '';
+   }
+
+   workSection.addEventListener('click', (e) => {
+      e.preventDefault();
+      const target = e.target;
+      if (target && target.classList.contains('preview')) {
+         popupDisplayFlex();
+         const path = target.parentNode.getAttribute('href');
+         bigImage.setAttribute('src', path);
+      }
+
+      if (target && target.matches('div.popup')) {
+         popupDisplayNone();
+      }
+   });
+};
