58209f42a94d3b7f00f893d86fd8ef48645a2d49
Dmitrij Tolstov <tolstov.dmitrij42@gmail.com>
2022-05-18 13:46:22 +0400

    adds function scrolls

--- src/js/modules/images.js
+++ src/js/modules/images.js
@@ -2,6 +2,7 @@ export const images = () => {
    const imgPopup = document.createElement('div'),
       workSection = document.querySelector('.works'),
       bigImage = document.createElement('img');
+   const scroll = calcScroll();
 
    imgPopup.classList.add('popup');
    workSection.append(imgPopup);
@@ -14,10 +15,12 @@ export const images = () => {
    function popupDisplayFlex() {
       imgPopup.style.display = 'flex';
       document.body.style.overflow = 'hidden';
+      document.body.style.marginRight = `${scroll}px`;
    }
    function popupDisplayNone() {
       imgPopup.style.display = 'none';
       document.body.style.overflow = '';
+      document.body.style.marginRight = `0px`;
    }
 
    workSection.addEventListener('click', (e) => {
@@ -31,6 +34,27 @@ export const images = () => {
       }
 
       if (target && target.matches('div.popup')) {
+         popupDisplayNone();
+      }
+   });
+
+   function calcScroll() {
+      const div = document.createElement('div');
+      div.style.width = '50px';
+      div.style.height = '50px';
+      div.style.overflowY = 'scroll';
+      div.style.visibility = 'hidden';
+      document.body.appendChild(div);
+      const scrollWidth = div.offsetWidth - div.clientWidth;
+      div.remove();
+
+      return scrollWidth;
+   }
+
+   window.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') {
+         document.body.style.marginRight = `0px`;
+
          popupDisplayNone();
       }
    });
